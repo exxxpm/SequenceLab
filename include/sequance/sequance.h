35c356b9c9c83771d2410afa61b823400a2e7ab2
exxxpm <127376127+exxxpm@users.noreply.github.com>
2023-03-15 01:49:59 +0400

    add sequance.h

--- include/sequance/sequance.h
+++ include/sequance/sequance.h
@@ -1,3 +1,29 @@
 #pragma once
+namespace sequance {
+	enum SequanseItemType {
+		ARITHMETIC,
+		GEOMETRIC,
+	};
 
-int sum_stub(int lhs, int rhs);
+	class SequanseItem {
+		private:
+			SequanseItemType function_type;
+			double first_number_progression;
+			double step_progression;
+			double denominator_progression;
+		public:
+			SequanseItem();
+			SequanseItem(SequanseItemType function_type, double first_number_progression, double denominator_progression);
+			double calc_nth_elem(SequanseItemType function_type, int n);
+			double calc_summ_nth_elem(SequanseItemType function_type, int n);
+	};
+
+	class SequanseContaier {
+		public:
+			static const int DATASIZE = 10;
+		private:
+			SequanseItem sequance_array[DATASIZE];
+			int user_size;
+		pu
+	};
+}
