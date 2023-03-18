d6c212d0446e7a2d98f03d41f3be1707cc6b0021
exxxpm <127376127+exxxpm@users.noreply.github.com>
2023-03-19 01:52:02 +0400

    making sequense.cc

--- include/sequense/sequense.h
+++ include/sequense/sequense.h
@@ -13,17 +13,25 @@ namespace sequance {
 			double denominator_progression;
 		public:
 			SequanseItem();
+			SequanseItemType get_type() const;
 			SequanseItem(SequanseItemType function_type, double first_number_progression, double denominator_progression);
-			double calc_nth_elem(SequanseItemType function_type, int n);
-			double calc_summ_nth_elem(SequanseItemType function_type, int n);
+			double calc_nth_elem(int n);
+			double calc_summ_nth_elem(int n);
 	};
 
-	class SequanseContaier {
+	class SequanseContainer {
 		public:
 			static const int DATASIZE = 10;
 		private:
 			SequanseItem sequance_array[DATASIZE];
 			int user_size;
-		pu
+		public:
+			SequanseContainer();
+			int get_size() const;
+			void remove(int index);
+			void insert(int index, SequanseItem s);
+			SequanseItem operator[](int index) const;
 	};
+
+	int max_summ_nth_elem(const SequanseContainer& sequence, int n);
 }
--- src/sequance.cc
+++
@@ -1,5 +0,0 @@
-#include <sequance/sequance.h>
-
-int sum_stub(int lhs, int rhs) {
-    return lhs + rhs;
-}
