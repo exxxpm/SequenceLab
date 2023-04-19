ec7ffcee23e15df4728f638f747db5bac2bbcaf0
exxxpm <127376127+exxxpm@users.noreply.github.com>
2023-04-20 00:47:36 +0400

    replacement static to dynamic

--- include/sequence/sequence.h
+++ include/sequence/sequence.h
@@ -17,20 +17,30 @@ namespace sequence {
 			SequanceItemType get_type() const;
 			double calc_nth_elem(int n);
 			double calc_summ_nth_elem(int n);
+			//double set_first_number_progression();
+			//double set_step_progression();
+			//double set_denominator_progression();
+			//friend std::ostream& operator<<(std::ostream& out, const SequanceItemType& function_type);
+			//friend std::ostream& operator<<(std::ostream& out, const SequanceItem& sequance);
 	};
 
 	class SequanceContainer {
-		public:
-			static const int DATASIZE = 10;
 		private:
-			SequanceItem sequence_array[DATASIZE];
+			SequanceItem** sequence_array;
 			int user_size;
 		public:
-			SequanceContainer();
+			~SequanceContainer();
+			SequanceContainer(int size);
+			SequanceContainer(const SequanceContainer& arr);
+			SequanceContainer() : user_size(0), sequence_array(nullptr) {};
 			int get_size() const;
 			void remove(int index);
-			void insert(int index, SequanceItem s);
+			void insert(int index, SequanceItem& elem);
+			void swap(SequanceContainer& arr);
 			SequanceItem operator[](int index) const;
+			SequanceItem& operator[](int index);
+			SequanceContainer& operator=(SequanceContainer arr);
+
 	};
 
 	int max_summ_nth_elem(const SequanceContainer& sequence, int n);
--- src/sequence_container.cc
+++ src/sequence_container.cc
@@ -4,53 +4,104 @@
 using namespace sequence;
 using namespace std;
 
-SequanceContainer::SequanceContainer() : user_size(0) {}
+SequanceContainer::~SequanceContainer() {
+    for (int i = 0; i < user_size; i++) {
+        delete sequence_array[i];
+    }
+    delete[] sequence_array;
+}
+
+SequanceContainer::SequanceContainer(const SequanceContainer& arr) {
+    user_size = arr.user_size;
+    sequence_array = new SequanceItem * [user_size];
+    for (int i = 0; i < user_size; i++) {
+        sequence_array[i] = new SequanceItem(*arr.sequence_array[i]);
+    }
+}
+
+SequanceContainer::SequanceContainer(int size) {
+    if (size < 0) throw std::invalid_argument("Size of array should be positive");
+    user_size = size;
+    sequence_array = new SequanceItem * [size];
+    for (int i = 0; i < size; i++) {
+        sequence_array[i] = new SequanceItem();
+    }
+}
+
+SequanceItem& SequanceContainer::operator[](int index) {
+    if (index < 0 || index >= user_size) {
+        throw out_of_range("Index out of range.");
+    }
+    return *(sequence_array[index]);
+}
+
+SequanceItem SequanceContainer::operator[](int index) const {
+    if (index < 0 || index >= user_size) {
+        throw out_of_range("Index out of range.");
+    }
+    return *(sequence_array[index]);
+}
+
+SequanceContainer& ::SequanceContainer::operator=(SequanceContainer arr) {
+    this->swap(arr);
+    return *this;
+}
 
 int SequanceContainer::get_size() const {
     return user_size;
 }
 
-SequanceItem SequanceContainer::operator[](int index) const {
-    if (index < 0 || user_size <= index) {
-        throw runtime_error("Index out of range.");
-    }
-    return sequence_array[index];
+void SequanceContainer::swap(SequanceContainer& arr) {
+    std::swap(user_size, arr.user_size);
+    std::swap(sequence_array, arr.sequence_array);
 }
 
-void SequanceContainer::insert(int index, SequanceItem s) {
-    if (user_size == DATASIZE) {
-        throw runtime_error("Full capacity reached.");
-    }
-    for (int i = user_size - 1; i >= index; i--) {
-        sequence_array[i + 1] = sequence_array[i];
+void SequanceContainer::insert(int index, SequanceItem& elem) {
+    if (index > user_size || index < 0) { throw out_of_range("Invalid index");}
+    SequanceItem** new_sequence_array = new SequanceItem * [user_size + 1];
+
+    for (int i = 0; i < index; i++) {
+        new_sequence_array[i] = sequence_array[i];
     }
 
-    sequence_array[index] = s;
+    new_sequence_array[index] = new SequanceItem(elem);
     ++user_size;
+
+    for (int i = index + 1; i < user_size; i++) {
+        new_sequence_array[i] = sequence_array[i - 1];
+    }
+
+    delete[] sequence_array;
+    sequence_array = new_sequence_array;
 }
+
 
 void SequanceContainer::remove(int index) {
-    if (index < 0 || index >= user_size) {
-        throw out_of_range("Invalid index.");
-    }
-    for (int i = index; i < user_size - 1; i++) {
-        sequence_array[i] = sequence_array[i + 1];
-    }
-    --user_size;
+    if (index < 0 || index >= user_size) { throw out_of_range("Invalid index."); }
+
+    SequanceItem** ptr = new SequanceItem * [user_size - 1];
+
+    std::copy(sequence_array, sequence_array + index, ptr);
+    std::copy(sequence_array + index + 1, sequence_array + user_size, ptr + index);
+
+    delete sequence_array[index];
+    delete[] sequence_array;
+
+    sequence_array = ptr;
+    user_size--;
 }
 
-int sequence::max_summ_nth_elem(const SequanceContainer& sequence, int n) {
-    int last_index = -1;
-    int max_value = 0;
 
+int sequence::max_summ_nth_elem(const SequanceContainer& sequence, int n) {
+    int max_index = -1;
+    double max_value = 0;
     const auto size = sequence.get_size();
     for (int i = 0; i < size; ++i) {
         const auto value = sequence[i].calc_summ_nth_elem(n);
-        if (last_index == -1 || value >= max_value) {
-            last_index = i;
+        if (value >= max_value) {
+            max_index = i;
             max_value = value;
         }
     }
-
-    return last_index;
+    return max_index;
 }
